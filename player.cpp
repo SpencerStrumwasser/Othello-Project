c96fb174db519be8c3de905998bc8926e21d3cdc
AbrahamHussain <abrahamhussain@caltech.edu>
2016-03-07 20:41:53 -0800

    hello

--- player.cpp
+++ player.cpp
@@ -12,13 +12,14 @@
  */
 Player::Player(Side side) {
     // Will be set to true in test_minimax.cpp.
-    testingMinimax = false;
-
+    testingMinimax = false; 
+    
     /* 
      * TODO: Do any initialization you need to do here (setting up the board,
      * precalculating things, etc.) However, remember that you will only have
      * 30 seconds.
      */
+     
 }
 
 /*
